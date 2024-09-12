1bccaa1338fccd4f47777f51c0563a085d67a2cc
Marius Meschter <git@onestay.moe>
2024-09-12 13:25:56 +0200

    poetry -> uv and requests -> httpx

--- cf_dyndns/api.py
+++ cf_dyndns/api.py
@@ -1,39 +1,44 @@
 from __future__ import annotations
 
 import logging
-from datetime import datetime
+from datetime import datetime  # noqa: TCH003
 from typing import Any
 from urllib.parse import urlencode
 
-import requests
-from pydantic import BaseModel, Field
+import httpx
+from pydantic import BaseModel, Field, ValidationError
 
 logger = logging.getLogger(__name__)
 
-_BASE_URL = "https://api.cloudflare.com/client/v4"
+_BASE_URL = 'https://api.cloudflare.com/client/v4'
 
 
 class CloudflareRequestError(Exception):
-    def __init__(self, res: requests.Response) -> None:
-        msg = "cf error"
-        errors = res.json()["errors"]
+    def __init__(self, res: httpx.Response) -> None:
+        msg = 'cf error'
+        errors = res.json()['errors']
         errors = [f"{error['message']} ({error['code']})" for error in errors]
-        error_msg = "\t\n".join(errors)
-        msg = f"{res.url} failed with {res.status_code}: {error_msg}"
+        error_msg = '\t\n'.join(errors)
+        msg = f'{res.url} failed with {res.status_code}: {error_msg}'
         super().__init__(msg)
+
+
+class CloudflareResponseParseError(Exception):
+    pass
 
 
 class CloudflareZoneNotFoundError(Exception):
     pass
+
 
 class CloudflareRecordNotFoundError(Exception):
     pass
 
 
 def _make_url(endpoint: str, base: str = _BASE_URL) -> str:
-    base = base.rstrip("/")
-    if not endpoint.startswith("/"):
-        endpoint = "/" + endpoint
+    base = base.rstrip('/')
+    if not endpoint.startswith('/'):
+        endpoint = '/' + endpoint
 
     return base + endpoint
 
@@ -41,88 +46,95 @@ def _make_url(endpoint: str, base: str = _BASE_URL) -> str:
 class CloudflareClient:
     def __init__(self, api_key: str, timeout: int = 10) -> None:
         self.api_key = api_key
-        self.client = requests.session()
-        self.client.headers.update({"Authorization": f"Bearer {self.api_key}"})
-        self.timeout = timeout
+        self.client = httpx.Client(timeout=timeout, http2=True)
+        self.client.headers.update({'Authorization': f'Bearer {self.api_key}'})
 
     def do_put(
         self,
         endpoint: str,
         params: dict[str, str] | None = None,
-        body: str | None = None,
+        body: dict[str, Any] | None = None,
     ):
         url = _make_url(endpoint)
         if params is not None:
-            url = url + f"?{urlencode(params)}"
+            url = url + f'?{urlencode(params)}'
 
-        response = self.client.put(url, data=body, timeout=self.timeout)
+        response = self.client.put(url, json=body)
 
         json_response = response.json()
-        if not json_response["success"]:
+        if not json_response['success']:
             raise CloudflareRequestError(response)
 
-        return json_response["result"]
+        return json_response['result']
 
     def do_post(
         self,
         endpoint: str,
         params: dict[str, str] | None = None,
-        body: str | None = None,
+        body: dict[str, Any] | None = None,
     ):
         url = _make_url(endpoint)
         if params is not None:
-            url = url + f"?{urlencode(params)}"
+            url = url + f'?{urlencode(params)}'
 
-        response = self.client.post(url, data=body, timeout=self.timeout)
+        response = self.client.post(url, json=body)
 
         json_response = response.json()
-        if not json_response["success"]:
+        if not json_response['success']:
+            print(body)
             raise CloudflareRequestError(response)
 
-        return json_response["result"]
+        return json_response['result']
 
-    def do_get(
-        self, endpoint: str, params: dict[str, str] | None = None
-    ) -> list[dict[Any, Any]]:
+    def do_get(self, endpoint: str, params: dict[str, str] | None = None) -> list[dict[Any, Any]]:
         url = _make_url(endpoint)
-        if params is not None:
-            url = url + f"?{urlencode(params)}"
 
-        response = self.client.get(url, timeout=self.timeout)
+        response = self.client.get(url, params=params)
 
         json_response = response.json()
-        if not json_response["success"]:
+        if not json_response['success']:
             raise CloudflareRequestError(response)
 
-        return json_response["result"]
+        return json_response['result']
+
+
+_zone_cache: dict[str, Zone] = {}
 
 
 class Zone(BaseModel):
     created_on: datetime
-    zone_id: str = Field(alias="id")
+    zone_id: str = Field(alias='id')
     name: str
 
     @staticmethod
     def list_zones(cf: CloudflareClient) -> list[Zone]:
-        return [Zone(**result) for result in cf.do_get("zones")]
+        return [Zone(**result) for result in cf.do_get('zones')]
 
     @staticmethod
     def get_by_name(hostname: str, cf: CloudflareClient) -> Zone:
-        response = cf.do_get("zones", {"name": hostname})
+        if hostname in _zone_cache:
+            return _zone_cache[hostname]
+        response = cf.do_get('zones', {'name': hostname})
         try:
             zone = response[0]
         except IndexError as e:
             msg = f"zone with name '{hostname}' not found"
             raise CloudflareZoneNotFoundError(msg) from e
 
-        return Zone(**zone)
+        try:
+            parsed_zone = Zone.model_validate(zone)
+        except ValidationError as err:
+            msg = f'Could not construct a zone from response:\n{zone}'
+            raise CloudflareResponseParseError(msg) from err
+
+        return parsed_zone
 
 
 class CreateDNSRecord(BaseModel):
     content: str
     name: str
     proxied: bool | None = None
-    record_type: str = Field(alias="type")
+    record_type: str = Field(alias='type')
     comment: str | None = None
     tags: list[str] | None = None
     ttl: int | None = None
@@ -130,47 +142,55 @@ class CreateDNSRecord(BaseModel):
 
 class DNSRecord(BaseModel):
     content: str
-    record_type: str = Field(alias="type")
-    record_id: str = Field(alias="id")
+    record_type: str = Field(alias='type')
+    record_id: str = Field(alias='id')
     comment: str | None
     zone_id: str
     zone_name: str
     ttl: int
     created_on: datetime
-    locked: bool
 
-    @staticmethod
-    def create(create_dns_record: CreateDNSRecord, zone_id: str, cf: CloudflareClient):
-        request_body = create_dns_record.model_dump_json(
-            by_alias=True, exclude_unset=True
-        )
+    @classmethod
+    def parse_record(cls, resp: Any) -> DNSRecord:
+        try:
+            parsed_record = cls.model_validate(resp)
+        except ValidationError as err:
+            msg = f'Could not construct DNSRecord from response:\n{resp}'
+            raise CloudflareResponseParseError(msg) from err
+        return parsed_record
 
-        return DNSRecord(
-            **cf.do_post(f"/zones/{zone_id}/dns_records", body=request_body)
-        )
+    @classmethod
+    def create(cls, create_dns_record: CreateDNSRecord, zone_id: str, cf: CloudflareClient):
+        request_body = create_dns_record.model_dump(by_alias=True, exclude_unset=True)
 
-    @staticmethod
-    def list_all_records(zone_id: str, cf: CloudflareClient):
-        return [
-            DNSRecord(**result)
-            for result in cf.do_get(f"/zones/{zone_id}/dns_records")
-        ]
+        resp = cf.do_post(f'/zones/{zone_id}/dns_records', body=request_body)
+        return cls.parse_record(resp)
 
-    @staticmethod
-    def get_record_by_name_and_type(zone_id: str, name: str, record_type: str, cf: CloudflareClient) -> DNSRecord:
-        response = cf.do_get(f"zones/{zone_id}/dns_records", {"name": name, "type": record_type})
+    @classmethod
+    def list_all_records(cls, zone_id: str, cf: CloudflareClient) -> list[DNSRecord]:
+        responses = cf.do_get(f'/zones/{zone_id}/dns_records')
+
+        return [cls.parse_record(resp) for resp in responses]
+
+    @classmethod
+    def get_record_by_name_and_type(
+        cls, zone_id: str, name: str, record_type: str, cf: CloudflareClient
+    ) -> DNSRecord:
+        response = cf.do_get(f'zones/{zone_id}/dns_records', {'name': name, 'type': record_type})
         try:
             record = response[0]
         except IndexError as e:
             msg = f"record with name '{name}' and type '{record_type}' not found"
             raise CloudflareRecordNotFoundError(msg) from e
 
-        return DNSRecord(**record)
+        return cls.parse_record(record)
 
-    @staticmethod
-    def update_record(zone_id: str, record_id: str, create_dns_record: CreateDNSRecord, cf: CloudflareClient) -> DNSRecord:
-        request_body = create_dns_record.model_dump_json(by_alias=True, exclude_unset=True)
+    @classmethod
+    def update_record(
+        cls, zone_id: str, record_id: str, create_dns_record: CreateDNSRecord, cf: CloudflareClient
+    ) -> DNSRecord:
+        request_body = create_dns_record.model_dump(by_alias=True, exclude_unset=True)
 
-        return DNSRecord(
-            **cf.do_put(f"/zones/{zone_id}/dns_records/{record_id}", body=request_body)
-        )
+        resp = cf.do_put(f'/zones/{zone_id}/dns_records/{record_id}', body=request_body)
+
+        return cls.parse_record(resp)
--- cf_dyndns/main.py
+++ cf_dyndns/main.py
@@ -1,8 +1,7 @@
-from __future__ import annotations
-
 import argparse
 import logging
 import signal
+import tomllib
 from dataclasses import dataclass
 from enum import StrEnum
 from ipaddress import IPv4Address, IPv6Address, ip_address
@@ -11,8 +10,7 @@ from threading import Event
 from types import FrameType
 from typing import Any
 
-import requests
-import tomllib
+import httpx
 
 from .api import (
     CloudflareClient,
@@ -26,35 +24,34 @@ from .api import (
 logger = logging.getLogger(__name__)
 
 parser = argparse.ArgumentParser(
-    prog="cf_dyndns", description="Dynamically update Cloudflare DNS records"
+    prog='cf_dyndns', description='Dynamically update Cloudflare DNS records'
 )
 
-parser.add_argument("-c", "--config", required=True, help="Path to config file")
+parser.add_argument('-c', '--config', required=True, help='Path to config file')
 parser.add_argument(
-    "-t",
-    "--timeout",
+    '-t',
+    '--timeout',
     default=60,
     type=int,
-    help="How long to wait before re-running. Set to 0 to only run once.",
+    help='How long to wait before re-running. Set to 0 to only run once.',
 )
 parser.add_argument(
-    "-d",
-    "--dry-run",
+    '-d',
+    '--dry-run',
     default=False,
-    action="store_true",
-    help="Do everything except for updating the records",
+    action='store_true',
+    help='Do everything except for updating the records',
 )
 
-parser.add_argument(
-    "-v", "--verbose", default=False, action="store_true", help="Verbose logging"
-)
+parser.add_argument('-v', '--verbose', default=False, action='store_true', help='Verbose logging')
+parser.add_argument('-n', '--create', help='Creating missing DNS Records', action='store_true')
 
 run = Event()
 
 
 def handler(signum: int, _: FrameType | None):
     if signum == signal.SIGINT:
-        logger.info("Got SIGINT. Shutting down...")
+        logger.info('Got SIGINT. Shutting down...')
         run.set()
 
 
@@ -62,8 +59,8 @@ signal.signal(signal.SIGINT, handler)
 
 
 class IPVer(StrEnum):
-    v4 = "ipv4"
-    v6 = "ipv6"
+    v4 = 'ipv4'
+    v6 = 'ipv6'
 
 
 @dataclass
@@ -74,34 +71,39 @@ class WebsiteConfig:
     ipv6: bool
 
     @staticmethod
-    def from_config(config: dict[str, Any]) -> list[WebsiteConfig]:
+    def from_config(config: dict[str, Any]) -> list['WebsiteConfig']:
         return [WebsiteConfig(k, **config[k]) for k in config]
 
 
-def get_ip(ip_ver: IPVer) -> IPv6Address | IPv4Address:
-    url = f"https://{ip_ver}.icanhazip.com"
-    logger.debug("requesting %s from %s", ip_ver, url)
-    response = requests.get(url, timeout=5)
-    logger.debug("%s responded with %s", url, response.text)
+def get_ip(ip_ver: IPVer) -> IPv6Address | IPv4Address | None:
+    url = f'https://{ip_ver}.icanhazip.com'
+    logger.debug('requesting %s from %s', ip_ver, url)
+    try:
+        response = httpx.get(url, timeout=5)
+    except httpx.HTTPError:
+        logger.warning('Failed to get %s. Skipping', ip_ver)
+        return None
+    logger.debug('%s responded with %s', url, response.text)
     return ip_address(response.text.strip())
 
 
 def get_ipv4() -> IPv4Address:
     ip = get_ip(IPVer.v4)
-    logger.debug("Got ipv4: %s", ip)
+    logger.debug('Got ipv4: %s', ip)
     if not isinstance(ip, IPv4Address):
-        txt = f"Expected IPv4Address but got {type(ip)}"
+        txt = f'Expected IPv4Address but got {type(ip)}'
         raise TypeError(txt)
 
     return ip
 
 
-def get_ipv6() -> IPv6Address:
+def get_ipv6() -> IPv6Address | None:
     ip = get_ip(IPVer.v6)
-    logger.debug("Got ipv6: %s", ip)
+    logger.debug('Got ipv6: %s', ip)
     if not isinstance(ip, IPv6Address):
-        txt = f"Expected IPv6Address but got {type(ip)}"
-        raise TypeError(txt)
+        txt = f'Expected IPv6Address but got {type(ip)}'
+        logger.warning(txt)
+        return None
 
     return ip
 
@@ -110,45 +112,60 @@ def update_website(
     cf: CloudflareClient,
     website: WebsiteConfig,
     ipv4: IPv4Address,
-    ipv6: IPv6Address,
-    dry_run: bool,
+    ipv6: IPv6Address | None,
+    dry_run: bool,  # noqa: FBT001
+    create_missing_records: bool = False,
 ):
-    logger.info(
-        "updating %s (ipv4: %s, ipv6: %s)", website.name, website.ipv4, website.ipv6
-    )
+    logger.info('updating %s (ipv4: %s, ipv6: %s)', website.name, website.ipv4, website.ipv6)
     try:
         zone_id = Zone.get_by_name(website.zone, cf).zone_id
     except CloudflareZoneNotFoundError:
-        logger.warning("%s zone not found", website.zone)
+        logger.warning('%s zone not found', website.zone)
         return
 
     for ip_ver in [IPVer.v4, IPVer.v6]:
         if ip_ver == IPVer.v4 and not website.ipv4:
-            logger.info("Skipping ipv4")
+            logger.info('Skipping ipv4')
+            continue
+
+        if ip_ver == IPVer.v6 and ipv6 is None:
+            logger.warning('No IPv6 address available. Skipping')
             continue
 
         if ip_ver == IPVer.v6 and not website.ipv6:
-            logger.info("Skipping ipv6")
+            logger.info('Skipping ipv6')
             continue
 
         ip = ipv4 if ip_ver == IPVer.v4 else ipv6
-        record_type = "A" if ip_ver == IPVer.v4 else "AAAA"
+        record_type = 'A' if ip_ver == IPVer.v4 else 'AAAA'
 
         try:
-            record = DNSRecord.get_record_by_name_and_type(
-                zone_id, website.name, record_type, cf
-            )
+            record = DNSRecord.get_record_by_name_and_type(zone_id, website.name, record_type, cf)
         except CloudflareRecordNotFoundError:
-            logger.warning("%s record not found for %s", record_type, website.name)
+            logger.debug('%s record not found for %s. Creating...', record_type, website.name)
+            if create_missing_records:
+                logger.info('%s ')
+                new_records = CreateDNSRecord(
+                    content=str(ip),
+                    name=website.name,
+                    proxied=False,
+                    type=record_type,
+                    comment='Created by dnsupdate (https://github.com/onestay/cf-dyndns)',
+                )
+
+                if dry_run:
+                    logger.info('dry run set. Not creating')
+                    continue
+                DNSRecord.create(new_records, zone_id, cf)
+            else:
+                logger.warning('%s record not found for %s', record_type, website.name)
             continue
 
-        logger.debug(
-            "%s record found for %s: %s", record_type, website.name, record.content
-        )
+        logger.debug('%s record found for %s: %s', record_type, website.name, record.content)
 
         if record.content == str(ip):
             logger.debug(
-                "%s record for %s is up to date (%s)",
+                '%s record for %s is up to date (%s)',
                 record_type,
                 website.name,
                 record.content,
@@ -156,18 +173,16 @@ def update_website(
             continue
 
         logger.info(
-            "%s record for %s is out of date\n\tfound: %s\n\texpected:%s",
+            '%s record for %s is out of date\n\tfound: %s\n\texpected:%s',
             record_type,
             website.name,
             record.content,
             ip,
         )
-        updated_record = CreateDNSRecord(
-            content=str(ip), name=website.name, type=record_type
-        )
+        updated_record = CreateDNSRecord(content=str(ip), name=website.name, type=record_type)
 
         if dry_run:
-            logger.info("dry run set. Not updating")
+            logger.info('dry run set. Not updating')
             continue
 
         DNSRecord.update_record(zone_id, record.record_id, updated_record, cf)
@@ -180,29 +195,29 @@ def main():
 
     config_file = Path(args.config)
 
-    with config_file.open("rb") as cf:
+    with config_file.open('rb') as cf:
         config = tomllib.load(cf)
 
-    general = config.pop("general")
-    logger.debug("Creating Cloudflare connection")
-    cf = CloudflareClient(general["cloudflare_api_key"])
+    general = config.pop('general')
+    logger.debug('Creating Cloudflare connection')
+    cf = CloudflareClient(general['cloudflare_api_key'])
 
     websites = WebsiteConfig.from_config(config)
     while not run.is_set():
-        logger.debug("Getting IP addresses")
+        logger.debug('Getting IP addresses')
         ipv4 = get_ipv4()
         ipv6 = get_ipv6()
 
         for website in websites:
-            update_website(cf, website, ipv4, ipv6, args.dry_run)
+            update_website(cf, website, ipv4, ipv6, args.dry_run, args.create)
 
         if args.timeout == 0:
             break
 
-        logger.debug("Sleeping for %s seconds", args.timeout)
+        logger.debug('Sleeping for %s seconds', args.timeout)
 
         run.wait(args.timeout)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
